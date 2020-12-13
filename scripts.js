50d41c9a91b5e7b185059988200bf6c9bc13ebbb
Nina Spray <ninaspray@gmail.com>
2020-12-13 17:53:58 +0000

    Added JavaScript Pop Up Boxes to Contact Me - Portfolio Work

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 function validation() {
-    alert("working");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+
+    if (name.length < 5) {
+        text = "Please Enter Valid Name"
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        text = "Please Enter Valid Email"
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if (message.length <= 140) {
+        text = "Please Enter More than 140 Characters"
+        error_message.innerHTML = text;
+        return false;
+    }
+    alert("Form Submitted Successfully!")
+    return true;
+
 }
